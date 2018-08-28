e066a851f4e07f0762a7c3ad5ad731a78a977fb2
Andrew Mussey <git@amussey.com>
2018-08-28 15:51:57 -0700

    Add cat caching (cat-ching?)

--- fetch_cats.py
+++ fetch_cats.py
@@ -1,6 +1,8 @@
 from lxml import html
 import click
 import collections
+import json
+import os
 import progressbar
 import re
 import requests
@@ -10,21 +12,31 @@ Weight = collections.namedtuple('Weight', 'str int lb oz')
 Age = collections.namedtuple('Age', 'str int')
 
 URL_BASE = 'https://www.sfspca.org'
+CAT_CACHE_FILE = '.cat-cache.json'
 
 
 class Cat(object):
-    def __init__(self, url, url_base=URL_BASE):
+    def __init__(self, url, url_base=URL_BASE, cache=None):
         self.profile_url = url
         self.url_base = url_base
         self.url = '{}{}'.format(self.url_base, self.profile_url)
 
-        r = requests.get(self.url)
-        self.tree = html.fromstring(r.content)
+        if cache is not None:
+            self.parse_dict(cache)
+        else:
+            r = requests.get(self.url)
+            self.tree = html.fromstring(r.content)
 
-        self.name = self.tree.xpath("//div[contains(@class, 'field-name-title')]/h1/text()")[0].strip()
+            self.name = self.tree.xpath("//div[contains(@class, 'field-name-title')]/h1/text()")[0].strip()
 
-        self.parse_age()
-        self.parse_weight()
+            self.parse_age()
+            self.parse_weight()
+
+    def parse_dict(self, cache):
+        self.tree = None
+        self.name = cache['name']
+        self.age = Age(**cache['age'])
+        self.weight = Weight(**cache['weight'])
 
     def parse_age(self):
         age_xpath = self.tree.xpath("//div[contains(@class, 'field-name-field-animal-age')]/div/text()")
@@ -48,6 +60,14 @@ class Cat(object):
             self.weight = Weight(weight_str, (weight_lb * 16 + weight_oz), weight_lb, weight_oz)
         else:
             self.weight = Weight('Unknown', 0, 0, 0)
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'url': self.profile_url,
+            'age': dict(self.age._asdict()),
+            'weight': dict(self.weight._asdict()),
+        }
 
     def __str__(self):
         return '{: <40}: {}'.format(
@@ -80,17 +100,36 @@ def fetch_cat_urls():
     return cats
 
 
-def parse_cat():
-    pass
+def load_cat_cache():
+    if not os.path.isfile(CAT_CACHE_FILE):
+        return {}
+    with open(CAT_CACHE_FILE, 'r') as file:
+        return json.load(file)
+
+
+def save_cat_cache(cache):
+    with open(CAT_CACHE_FILE, 'w') as file:
+        file.write(json.dumps(cache))
 
 
 @click.command()
 def main():
     cat_urls = fetch_cat_urls()
     cats = []
-    for cat in progressbar.progressbar(cat_urls):
-        # print(Cat('/adoptions/pet-details/38019790'))
-        cats.append(Cat(cat))
+    cat_cache = load_cat_cache()
+
+    cat_cache_hit = [cat for cat in cat_urls if cat in cat_cache]
+    cat_cache_miss = [cat for cat in cat_urls if cat not in cat_cache]
+
+    for cat in progressbar.progressbar(cat_cache_hit):
+        cat_info = Cat(cat, cache=cat_cache[cat])
+        cats.append(cat_info)
+
+    for cat in progressbar.progressbar(cat_cache_miss):
+        cat_info = Cat(cat)
+        cat_cache[cat] = cat_info.to_dict()
+        save_cat_cache(cat_cache)
+        cats.append(cat_info)
 
     sorted_cats = sorted(cats, key=lambda k: -k.weight.int)
     print('Ranking:')
